f1ebaa39ba4e34628754155979347d1304bd0f23
Andrew <gskll@protonmail.com>
2020-12-01 16:57:28 +0100

    feat: add boilerplate routes for stream components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import StreamList from "./streams/StreamList";
+import StreamEdit from "./streams/StreamEdit";
+import StreamCreate from "./streams/StreamCreate";
+import StreamShow from "./streams/StreamShow";
+import StreamDelete from "./streams/StreamDelete";
 
 const App = () => {
-  return <h1>App</h1>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
