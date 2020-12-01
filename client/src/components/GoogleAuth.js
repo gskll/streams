0fb2689b5c495457c53fea90466fca7f41bde6dc
Andrew <gskll@protonmail.com>
2020-12-01 17:27:25 +0100

    feat: initialise google auth library

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "370481214679-337mstoma9qhd5e6s24jlopf2eppo4hq.apps.googleusercontent.com",
+        scope: "email",
+      });
+    });
+  }
+
+  render() {
+    return <div>GoogleAuth</div>;
+  }
+}
+
+export default GoogleAuth;
