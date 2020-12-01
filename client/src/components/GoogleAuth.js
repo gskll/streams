7242337f23b9f6d61acd9ea5fd9351a200e7e684
Andrew <gskll@protonmail.com>
2020-12-01 17:41:14 +0100

    feat: add google auth functionality

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,18 +1,35 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "370481214679-337mstoma9qhd5e6s24jlopf2eppo4hq.apps.googleusercontent.com",
-        scope: "email",
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "370481214679-337mstoma9qhd5e6s24jlopf2eppo4hq.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
 
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>not sure</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>signed in</div>;
+    } else {
+      return <div>not signed in</div>;
+    }
+  }
+
   render() {
-    return <div>GoogleAuth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
