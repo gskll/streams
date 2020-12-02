2eb16dacfb972ac923cef8564705bdf5d19475d0
Andrew <gskll@protonmail.com>
2020-12-02 15:27:37 +0100

    feat: link back to root path after stream created

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from "../apis/streams";
+import history from "../history";
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -27,6 +28,9 @@ export const createStream = (formValues) => async (dispatch, getState) => {
   const response = await streams.post("/streams", { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+
+  // Send user back to root once stream created/api response
+  history.push("/");
 };
 
 export const fetchStreams = () => async (dispatch) => {
