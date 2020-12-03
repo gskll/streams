7e1fa2aacc666eaa61281b406c28ff6a6bd91f7c
Andrew <gskll@protonmail.com>
2020-12-03 23:28:47 +0100

    feat: StreamDelete component navigation
    - change Route path to include stream id
    - Fetch stream on mount to handle data fetching
    - Change StreamList delete buttons to react-router Links
    - Customize delete modal content to include stream title

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,26 +1,50 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import history from "../../history";
 import Modal from "../Modal";
+import { fetchStream, deleteStream } from "../../actions";
 
-const StreamDelete = () => {
-  const actions = (
-    <React.Fragment>
-      <button className="ui negative button">Delete</button>
-      <button className="ui button">Cancel</button>
-    </React.Fragment>
-  );
-  return (
-    <div>
-      StreamDelete
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  renderActions() {
+    return (
+      <React.Fragment>
+        <button className="ui negative button">Delete</button>
+        <button className="ui button">Cancel</button>
+      </React.Fragment>
+    );
+  }
+
+  renderContent() {
+    if (!this.props.stream) {
+      return "Are you sure you want to delete this stream?";
+    }
+
+    return `Are you sure you want to delete this stream: ${this.props.stream.title}?`;
+  }
+
+  render() {
+    return (
       <Modal
         title="Delete Stream"
-        content="Are you sure you want to delete this stream?"
-        actions={actions}
+        content={this.renderContent()}
+        actions={this.renderActions()}
         onDismiss={() => history.push("/")}
       />
-    </div>
-  );
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id],
+  };
 };
 
-export default StreamDelete;
+export default connect(mapStateToProps, { fetchStream, deleteStream })(
+  StreamDelete
+);
