5cbccccc50875eff9e230a5cf5511eb7000cbbb2
Andrew <gskll@protonmail.com>
2020-12-03 22:50:23 +0100

    feat: display StreamDelete component
    - use Modal component to display StreamDelete
      - displayed when visiting /streams/delete
    - Modal navigates to root when background clicked
    - Displays relevant content for deleting a stream

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,7 +1,26 @@
 import React from "react";
 
+import history from "../../history";
+import Modal from "../Modal";
+
 const StreamDelete = () => {
-  return <h1>StreamDelete</h1>;
+  const actions = (
+    <React.Fragment>
+      <button className="ui negative button">Delete</button>
+      <button className="ui button">Cancel</button>
+    </React.Fragment>
+  );
+  return (
+    <div>
+      StreamDelete
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+        onDismiss={() => history.push("/")}
+      />
+    </div>
+  );
 };
 
 export default StreamDelete;
