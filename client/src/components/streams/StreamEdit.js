591349d3e0a740c37f60f6b64b8fb9f838b12c62
Andrew <gskll@protonmail.com>
2020-12-03 22:45:23 +0100

    feat: implement StreamEdit component
    - use StreamForm component to display editStreams component
    - initial values loaded with redux-form initialValues prop
    - pass id and formValues to editStreams action

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,17 +1,37 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchStream } from "../../actions";
+
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = (formValues) => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
-    return <h1>{this.props.stream.title}</h1>;
+
+    const { title, description } = this.props.stream;
+
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          initialValues={{
+            title,
+            description,
+          }}
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
   }
 }
 
@@ -21,4 +41,6 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
